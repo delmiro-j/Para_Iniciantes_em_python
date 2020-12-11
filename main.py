2d957e829979ce9bcc0681a58e7bd0331a2c8c98
Delmiro Ribeiro <delmirojunior2020@gmail.com>
2020-12-11 00:41:12 -0300

    adivinhando o numero

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def media(): # definindo a função
     #comandos de exemplos
     nota1 = float(input("minha primeira nota : ")) # adicionando valores a variavel
-    nota2 = float(input("minha segunda notA : "))
+    nota2 = float(input("minha segunda nota : "))
     return calcular_media(nota1,nota2) # retornando o valor da função
 
 def calcular_media(nota1,nota2): # pegando os valores por parâmetros
@@ -22,3 +22,19 @@ def calculando_aprovacao(media): # chamando uma função inteira por parâmetro
         print("ALUNO REPROVADO.")
 
 calculando_aprovacao(media()) # chamando a função.
+
+# EXERCICO DE ADVINHAÇÃO DE NUMERO
+# (NÃO HAVERÁ ESTRUTURAS COMPLEMENTARES NESSE REPOSITORIO)
+
+def main(): # definindo a função
+    numero_certo = 98764 # adicionado valor a variavel
+    numero_do_chute = float(input("Tente acerta o número certo : ")) # valor inserido pelo usuario
+
+    if numero_do_chute > numero_certo: # condicionais
+        print("Diminua o valor do chute")
+    elif numero_do_chute < numero_certo:
+        print("Aumente o valor do chute")
+    else:
+        print("Você acertou o valor do chute Parabens!")
+
+main()
